011a5aec437a4e92911964da4a8dff9fea1dd3bd
Morgan <titsworthmorgan@gmail.com>
2025-01-06 17:28:10 -0800

    fix

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -29,7 +29,7 @@ const social = [
     {
         name: 'Email',
         icon: 'email',
-        link: 'mailto:titsworthmorgan@gmail.com@gmail.com',
+        link: 'mailto:titsworthmorgan@gmail.com',
     },
 ]
 
